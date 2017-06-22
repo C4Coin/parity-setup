62bf5b154e3010dcc8807a6f2e6144ec81ed68fb
Jake Goulding <jake.goulding@gmail.com>
2017-06-22 18:42:33 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,52 @@ struct Wrapper<P: Serialize> {
     jsonrpc: &'static str,
     method: &'static str,
     params: P,
-    id: usize,
+    id: RpcId,
 }
 
+type RpcId = usize;
+type PersonalSendTransaction = Wrapper<PersonalSendTransactionParams>;
+
+impl PersonalSendTransaction {
+    fn new(params: PersonalSendTransactionParams, id: RpcId) -> Self {
+        Wrapper {
+            jsonrpc: JSONRPC_VERSION,
+            method: METHOD_NAME,
+            params, id,
+        }
+    }
+}
+
+type Password = &'static str;
+
 #[derive(Debug, Clone, PartialEq, Serialize)]
-struct PersonalSendTransactionParams {
+struct PersonalSendTransactionParams(Transaction, Password);
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+struct Transaction {
     from: String,
     to: String,
     value: String,
 }
-
-
-//"user"]
-
-//curl --data '{,"params":[,"id":0}' -H "Content-Type: application/json" -X POST localhost:8540
 
 fn main() {
     let from = "0x004ec07d2329997267Ec62b4166639513386F32E";
     let to = "0x00Bd138aBD70e2F00903268F3Db08f2D25677C9e";
     let value = "0xde0b6b3a7640000";
 
-    let param1 = PersonalSendTransactionParams {
+    let transaction = Transaction {
         from: from.into(),
         to: to.into(),
         value: value.into(),
     };
 
-    let param2 = "user";
+    let password = "user";
 
-    let params = (param1, param2);
+    let params = PersonalSendTransactionParams(transaction, password);
 
-    let rpc1 = Wrapper {
-        jsonrpc: JSONRPC_VERSION,
-        method: METHOD_NAME,
-        params: params,
-        id: 0,
-    };
-
-    let rpc = vec![rpc1];
+    let rpc = vec![
+        PersonalSendTransaction::new(params, 0),
+    ];
 
     println!("{}", serde_json::to_string(&rpc).unwrap());
 }
