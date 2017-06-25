4aef1e7a47462043c26705e89c35ede0f5d4b22f
Jake Goulding <jake.goulding@gmail.com>
2017-06-25 12:33:16 -0400

    Reformat and parameterize the analyze script

--- analyze-blocks.sh
+++ analyze-blocks.sh
@@ -1,4 +1,21 @@
-for i in $(seq 131 233); do curl -d "{\"method\":\"eth_getBlockByNumber\",\"params\":[\"$i\",true],\"id\":1,\"jsonrpc\":\"2.0\"}" -H "Content-Type: application/json" -X POST localhost:8541 -o "block-$i.json"; done
+#!/bin/bash
 
-for i in $(seq 131 233); do cat "block-$i.json" | jq -r '[.result.timestamp, (.result.transactions | length)] | @csv'; done
+set -eu
 
+start=$1
+end=$2
+
+parity_endpoint=${PARITY_ENDPOINT:-localhost:8541}
+
+for i in $(seq $start $end); do
+    curl -d "{\"method\":\"eth_getBlockByNumber\",\"params\":[\"$i\",true],\"id\":1,\"jsonrpc\":\"2.0\"}" \
+         -H "Content-Type: application/json" \
+         -X POST \
+         -o "block-$i.json" \
+         $parity_endpoint
+done
+
+for i in $(seq $start $end); do
+    cat "block-$i.json" | \
+        jq -r '[.result.timestamp, (.result.transactions | length)] | @csv'
+done
